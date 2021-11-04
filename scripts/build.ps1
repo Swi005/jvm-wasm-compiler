44607191d29f74828ac6610e39679fb09cf8f55d
Sander Wiig <sander.wiig@student.uib.no>
2021-11-04 11:54:41 +0100

    Reworked project filestructure

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,9 @@
-Get-ChildItem -Path ".\src" -Filter *.wat | 
+Get-ChildItem -Path ".\src\wasm" -Filter *.wat | 
 Foreach-Object {
     "Building {0}" -f $_.Name | Write-Output
     $file = $_ -replace ".wat"
-    $cmd = "wat2wasm .\src/{0}.wat -o .\bin/{0}.wasm" -f $file
+    $cmd = "wat2wasm .\src\wasm
+    \{0}.wat -o .\bin/{0}.wasm" -f $file
     Invoke-Expression $cmd 
     
 }
