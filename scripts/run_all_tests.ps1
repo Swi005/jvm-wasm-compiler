cb26ee6d96cdb04b1425c54257dbd3111c31c923
Sander Wiig <sander.wiig@student.uib.no>
2021-11-04 11:43:23 +0100

    Arrays are now a thing

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,9 +1,9 @@
 Get-ChildItem -Path ".\tests" -Filter *.js | 
-Foreach-Object {
+Foreach-Object 
+{
     Write-Output "--------------------------------------------------------------------------"
     "****Running {0}****" -f $_.Name | Write-Output
     Write-Output "--------------------------------------------------------------------------"
     $cmd = "node --unhandled-rejections=strict --experimental-modules --experimental-wasm-modules .\tests\{0}" -f $_
-    Invoke-Expression $cmd
-    
+    Invoke-Expression $cmd | Write-Output
 }
--- tests/array_test.js
+++ tests/array_test.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
 const bytes = fs.readFileSync('./bin/malloc.wasm');
 const bytes2 = fs.readFileSync('./bin/array.wasm');
-(async() =>{
-    const malloc = await WebAssembly.instantiate(bytes);
-    const array = await WebAssembly.instantiate(bytes2)
+console.log("UWU");
 
-    malloc.instance.exports.init();//Initalize the memory allocator
-    //WRITE TESTS HERE
-    console.log(new Uint8Array(malloc.exports.heap.buffer));
+WebAssembly.instantiate(bytes).then(module => {
+    var importObj = {
+        malloc: {
+            malloc: module.instance.exports.malloc,
+            free: module.instance.exports.free,
+            read_byte: module.instance.exports.read_byte,
+            write_byte: module.instance.exports.write_byte,
+            read_i32: module.instance.exports.read_i32,
+            write_i32: module.instance.exports.write_i32,
+            read_i64: module.instance.exports.read_i64,
+            write_i64: module.instance.exports.write_i64,
+            read_f32: module.instance.exports.read_f32,
+            write_f32: module.instance.exports.write_f32,
+            read_f64: module.instance.exports.read_f64,
+            write_f64: module.instance.exports.write_f64,
+        },
+    };
+    module.instance.exports.init()
+    return WebAssembly.instantiate(bytes2, importObj)
+}).then(module =>{
+    var arLength = 5;
+    var ptr = module.instance.exports.new_i32_array(arLength);
 
+    for (let index = 0; index < arLength; index++) {
+        module.instance.exports.set_elem_from_i32_array(ptr, index, 1);
+    }
+    
+    for (let index = 0; index < arLength+12; index++) {
+        console.log(module.instance.exports.get_elem_from_i32_array(ptr, index));
+    }
 });
